a2ada3be73c33c447dfef73c542c70d1e898fd8a
mdnriyas17 <mdnriyas@gmail.com>
2024-03-07 08:14:45 +0530

    routes add

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ app.use(express.json());
 
 dotenv.config();
 
+app.get("/", (req, res) => {
+    res.send("Hello World");
+})
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
